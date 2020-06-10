ae45518cc0647f9e2c28b96d690594ba2f502faa
Tom Banish <tombanish@gmail.com>
2020-06-10 01:59:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,6 +55,12 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
+  
   
   
   

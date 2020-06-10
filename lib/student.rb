4ddf49b6a67f47973f3a93145dc06a87cd4b9b16
Tom Banish <tombanish@gmail.com>
2020-06-10 02:25:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,8 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  attr_accessor :name, :grade
-  attr_reader :id
+  attr_accessor :name, :grade, :id
+  #attr_reader :id
   
   def initialize(id=nil, name, grade)
     @id = id

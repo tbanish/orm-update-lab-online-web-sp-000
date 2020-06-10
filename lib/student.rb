fe640db64b60eb182408af6b41fcb0d4a74443a0
Tom Banish <tombanish@gmail.com>
2020-06-10 01:50:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,6 +22,23 @@ class Student
       
     DB[:conn].execute(sql)
   end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+      SQL
+      
+    DB[:conn].execute(sql)
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 
 end

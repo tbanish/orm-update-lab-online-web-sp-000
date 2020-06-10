0386611646ead9eda11e2785c6cc9954b442c079
Tom Banish <tombanish@gmail.com>
2020-06-10 02:02:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
   end
   
   def self.new_from_db(row)
-    new_student = self.new(name, grade)
+    new_student = self.new(id, name, grade)
     new_student.id = row[0]
     new_student.name = row[1]
     new_studnet.grade = row[2]
